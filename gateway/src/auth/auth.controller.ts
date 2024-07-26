8c558e0c066cf1a7b1950373fa45a93fde64da1f
Gabriel <louis-gabriel.delaigue@poe.aelion.fr>
2024-07-26 09:42:47 +0200

    Ajout des paramètres créant des erreurs dans auth.service

--- gateway/src/auth/auth.controller.ts
+++ gateway/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthBodyType } from './models/auth-body.type';
 import { Observable, take } from 'rxjs';
